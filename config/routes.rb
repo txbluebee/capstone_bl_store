c5d30bd0e063f1c8e7ba51bd321b24eddee22b62
Brian Fan <kuomu.fan@yahoo.com>
2017-07-24 16:07:24 -0700

    add CRUD function to products

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,21 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'products/index'
+  end
+
+  namespace :admin do
+    get 'products/edit'
+  end
+
   root 'home#index'
 
   devise_for :users
 
   resources :products, only: [:index, :show]
+
+  namespace :admin do
+    resources :products
+  end
+
 end
