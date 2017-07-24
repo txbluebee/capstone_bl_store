a10729d828de7900701d0a42d95a700fcd3407b7
Brian Fan <kuomu.fan@yahoo.com>
2017-07-24 09:56:16 -0700

    add home page for user sign in

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170724162117) do
+ActiveRecord::Schema.define(version: 20170724164454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "quantity"
+    t.bigint "product_id"
+    t.bigint "cart_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
+    t.index ["product_id"], name: "index_cart_items_on_product_id"
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -41,4 +56,6 @@ ActiveRecord::Schema.define(version: 20170724162117) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cart_items", "carts"
+  add_foreign_key "cart_items", "products"
 end
---
+++ spec/factories/cart_items.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :cart_item do
+    quantity 1
+    product nil
+    cart nil
+  end
+end
