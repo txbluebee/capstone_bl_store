f252f142d1fc1298cee966db22b47f1b78620b26
Brian Fan <kuomu.fan@yahoo.com>
2017-07-25 11:33:57 -0700

    user can dismiss flash message

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,5 +1,29 @@
 class CartItemsController < ApplicationController
+  before_action :authenticate_user!
 
   def update
-  end  
+    @cart_item = current_cart.cart_items.find_by(product_id: params[:id])
+    if @cart_item.product.quantity >= cart_item_params[:quantity].to_i
+      @cart_item.update(cart_item_params)
+      redirect_to cart_path, info: 'successfully updated quantity'
+    else
+      redirect_to cart_path, danger: "Only #{@cart_item.product.quantity} left for #{@cart_item.product.title}"
+    end
+  end
+
+  def destroy
+    @cart_item = current_cart.cart_items.find_by(product_id: params[:id])
+    @product = @cart_item.product
+    @cart_item.destroy
+    redirect_to cart_path, warning: "successfully remove #{@product.title} from the cart"
+  end
+
+
+
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:quantity)
+  end
 end
--- app/helpers/cart_items_helper.rb
+++ app/helpers/cart_items_helper.rb
@@ -1,2 +1,5 @@
 module CartItemsHelper
+  def render_cart_total_price(cart)
+    cart.total_price
+  end
 end
