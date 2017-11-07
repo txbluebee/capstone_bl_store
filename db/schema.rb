6071dda0030cb22a3b541e235e0022c7616df055
Brian Fan <kuomu.fan@yahoo.com>
2017-11-06 21:59:12 -0800

    edit navbar

--- db/schema.rb
+++ db/schema.rb
@@ -82,6 +82,10 @@ ActiveRecord::Schema.define(version: 20170727154045) do
     t.datetime "updated_at", null: false
     t.boolean "is_admin", default: false
     t.string "username"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
